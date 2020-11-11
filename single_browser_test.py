2e88c5302e284b33e82025a6e7ac550ea751c737
Srivishnu Ayyagari <ayyagari@dalalstreet.ai>
2020-11-11 13:08:44 +0530

    Adds single test code

--- single_browser_test.py
+++ single_browser_test.py
@@ -2,14 +2,8 @@ from playwright import sync_playwright
 
 
 with sync_playwright() as p:
-    # iphone_11 = p.devices['iPhone 11 Pro']
     browser = p.webkit.launch(headless=False)
-    context = browser.newContext(
-        # **iphone_11,
-        locale='en-US',
-        geolocation={ 'longitude': 12.492507, 'latitude': 41.889938 },
-        permissions=['geolocation']
-    )
+    context = browser.newContext()
     page = context.newPage()
     page.goto('https://google.com')
     page.screenshot(path='screenshots/single-{browser}.png')
