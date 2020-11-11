21bf654f947d27bb57eb2c7b06d3f68c0b4b63e7
Srivishnu Ayyagari <ayyagari@dalalstreet.ai>
2020-11-11 13:12:40 +0530

    Adds comments

--- single_browser_test.py
+++ single_browser_test.py
@@ -2,9 +2,18 @@ from playwright import sync_playwright
 
 
 with sync_playwright() as p:
+    # Initialize the browser Chromium/FireFox/Webkit
     browser = p.webkit.launch(headless=False)
+
+    # Creating the context
     context = browser.newContext()
+
+    # Executing the test cases
     page = context.newPage()
     page.goto('https://google.com')
+
+    # Capturing the screenshots
     page.screenshot(path='screenshots/single-{browser}.png')
+
+    # Closing the browser
     browser.close()
